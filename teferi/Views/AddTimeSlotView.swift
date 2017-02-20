1a64692bb4a20f887f6ec591a7a09e809f7af3f9
Juxhin (Eugene) Bakalli <sofokli1@gmail.com>
2017-02-20 14:39:01 +0200

    :triangular_ruler: Undo radial gradient

--- teferi/Views/AddTimeSlotView.swift
+++ teferi/Views/AddTimeSlotView.swift
@@ -10,7 +10,7 @@ class AddTimeSlotView : UIView
     private var selectedCategory = Variable(Category.unknown)
     private var disposeBag : DisposeBag? = DisposeBag()
     
-    @IBOutlet private weak var blur : RadialGradientView!
+    @IBOutlet private weak var blur : UIView!
     @IBOutlet private weak var addButton : UIButton!
     private var wheel : Wheel<Category>!
 
@@ -48,6 +48,15 @@ class AddTimeSlotView : UIView
                       attributeSelector: self.toAttributes)
         
         wheel.addTarget(self, action: #selector(AddTimeSlotView.wheelChangedValue), for: .valueChanged)
+        
+        //Adds some blur to the background of the buttons
+        self.blur.frame = bounds;
+        let layer = CAGradientLayer()
+        layer.frame = self.blur.bounds
+        layer.colors = [ Color.white.withAlphaComponent(0).cgColor, Color.white.cgColor]
+        layer.locations = [0.0, 1.0]
+        self.blur.layer.addSublayer(layer)
+        self.blur.alpha = 0
         
         //Bindings
         self.categoryObservable
@@ -116,9 +125,6 @@ class AddTimeSlotView : UIView
         let options = isAdding ? UIViewAnimationOptions.curveEaseOut : UIViewAnimationOptions.curveEaseIn
 
         let delay : TimeInterval = 0
-        
-        blur.radialGradientCenterPoint = addButton.center
-        blur.radius = min(addButton.center.x, addButton.center.y)
         
         UIView.animate(withDuration: 0.2, delay: delay,
             options: options,
