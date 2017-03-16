76d1a24ad46a6644c892518a02e3ffd97f95e895
William Barbosa <william.sb@hotmail.com>
2017-03-16 10:35:37 -0300

    :sparkles: Initial pipeline objects

---
+++ teferi/Models/TemporaryTimeSlot.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+struct TemporaryTimeSlot
+{
+    let start : Date
+    let end : Date
+    let smartGuess : SmartGuess?
+    let category : Category
+    let location : Location?
+}
---
+++ teferi/Services/Protocols/TemporaryTimelineGenerator.swift
@@ -0,0 +1,4 @@
+protocol TemporaryTimelineGenerator
+{
+    func generateTemporaryTimeline() -> [TemporaryTimeSlot]
+}
