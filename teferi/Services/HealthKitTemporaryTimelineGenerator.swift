ad8f406d14eb81c2a22048e70ffd2140d89769c7
Juxhin (Eugene) Bakalli <sofokli1@gmail.com>
2017-03-20 17:00:11 +0200

    :construction: Rename

--- teferi/Services/HealthKitTemporaryTimelineGenerator.swift
+++ teferi/Services/HealthKitTemporaryTimelineGenerator.swift
@@ -1,7 +1,7 @@
 import Foundation
 import HealthKit
 
-class DefaultHealthKitTemporaryTimeLineGenerator : TemporaryTimelineGenerator
+class HealthKitTemporaryTimeLineGenerator : TemporaryTimelineGenerator
 {
     private let trackEventService : TrackEventService
     private let fastMovingSpeedThreshold : Double
