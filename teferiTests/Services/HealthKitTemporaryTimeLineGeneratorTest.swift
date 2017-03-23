502e6217efe21545394e8c6612ccc39bfeb4f72a
Juxhin (Eugene) Bakalli <sofokli1@gmail.com>
2017-03-23 11:34:47 +0200

    :construction: Fix tests

--- teferiTests/Services/HealthKitTemporaryTimeLineGeneratorTest.swift
+++ teferiTests/Services/HealthKitTemporaryTimeLineGeneratorTest.swift
@@ -36,15 +36,19 @@ class HealthKitTemporaryTimeLineGeneratorTest: XCTestCase
             .forEach { i, actualTimeSlot in compare(timeSlot: actualTimeSlot, to: expectedResult[i]) }
     }
     
-    func testContinuesTimeslotsFromSameCategoryAreMergedIntoOneTemporaryTimeslot()
+    func testContinuousHealthSamplesFromSameTypeAreMergedIntoOneTemporaryTimeslot()
     {
-        trackEventService.mockEvents = [(start: 00, end: 10, identifier: HKQuantityTypeIdentifier.distanceCycling.rawValue, quantity: 0),
-                                        (start: 10, end: 20, identifier: HKQuantityTypeIdentifier.distanceCycling.rawValue, quantity: 0),
-                                        (start: 20, end: 23, identifier: HKQuantityTypeIdentifier.distanceCycling.rawValue, quantity: 0),
-                                        (start: 23, end: 25, identifier: HKQuantityTypeIdentifier.distanceCycling.rawValue, quantity: 0)].map(toTrackEvent)
+        let sampleTuples : [HKSampleTuple] = [(start: 00, end: 10, identifier: HKQuantityTypeIdentifier.distanceCycling.rawValue, quantity: 0),
+                                              (start: 10, end: 20, identifier: HKQuantityTypeIdentifier.distanceCycling.rawValue, quantity: 0),
+                                              (start: 20, end: 23, identifier: HKQuantityTypeIdentifier.distanceCycling.rawValue, quantity: 0),
+                                              (start: 23, end: 25, identifier: HKQuantityTypeIdentifier.distanceCycling.rawValue, quantity: 0)]
         
-        let expectedResult = [(start: 0, category: .commute),
-                              (start: 25, category: .unknown)].map(toTempTimeSlot)
+        trackEventService.mockEvents = sampleTuples.map(toTrackEvent)
+        
+        let expectedResultTuples : [TempTimeSlotTuple] = [(start: 0, category: .commute),
+                                                          (start: 25, category: .unknown)]
+        
+        let expectedResult = expectedResultTuples.map(toTempTimeSlot)
         
         let generatedTimeslots = self.healthKitTemporaryTimelineGenerator.generateTemporaryTimeline()
         
@@ -55,20 +59,24 @@ class HealthKitTemporaryTimeLineGeneratorTest: XCTestCase
             .forEach { i, actualTimeSlot in compare(timeSlot: actualTimeSlot, to: expectedResult[i]) }
     }
     
-    func testContinuesTimeslotsFromDifferentCategoriesAreMergedIntoSeparateTemporaryTimeslot()
+    func testContinuousHealthSamplesFromDifferentTypesAreMergedIntoSeparateTemporaryTimeslot()
     {
-        trackEventService.mockEvents = [(start: 00, end: 10, identifier: HKQuantityTypeIdentifier.distanceCycling.rawValue, quantity: 200),
-                                        (start: 10, end: 20, identifier: HKQuantityTypeIdentifier.distanceCycling.rawValue, quantity: 200),
-                                        (start: 20, end: 30, identifier: HKQuantityTypeIdentifier.distanceCycling.rawValue, quantity: 200),
-                                        (start: 30, end: 40, identifier: HKQuantityTypeIdentifier.distanceCycling.rawValue, quantity: 200),
-                                        (start: 40, end: 50, identifier: HKQuantityTypeIdentifier.distanceWalkingRunning.rawValue, quantity: 200),
-                                        (start: 50, end: 60, identifier: HKQuantityTypeIdentifier.distanceWalkingRunning.rawValue, quantity: 200),
-                                        (start: 60, end: 70, identifier: HKQuantityTypeIdentifier.distanceWalkingRunning.rawValue, quantity: 200),
-                                        (start: 70, end: 80, identifier: HKQuantityTypeIdentifier.distanceWalkingRunning.rawValue, quantity: 200)].map(toTrackEvent)
+        let sampleTuples : [HKSampleTuple] = [(start: 00, end: 10, identifier: HKQuantityTypeIdentifier.distanceCycling.rawValue, quantity: 200),
+                                              (start: 10, end: 20, identifier: HKQuantityTypeIdentifier.distanceCycling.rawValue, quantity: 200),
+                                              (start: 20, end: 30, identifier: HKQuantityTypeIdentifier.distanceCycling.rawValue, quantity: 200),
+                                              (start: 30, end: 40, identifier: HKQuantityTypeIdentifier.distanceCycling.rawValue, quantity: 200),
+                                              (start: 40, end: 50, identifier: HKQuantityTypeIdentifier.distanceWalkingRunning.rawValue, quantity: 200),
+                                              (start: 50, end: 60, identifier: HKQuantityTypeIdentifier.distanceWalkingRunning.rawValue, quantity: 200),
+                                              (start: 60, end: 70, identifier: HKQuantityTypeIdentifier.distanceWalkingRunning.rawValue, quantity: 200),
+                                              (start: 70, end: 80, identifier: HKQuantityTypeIdentifier.distanceWalkingRunning.rawValue, quantity: 200)]
         
-        let expectedResult = [(start: 00, category: .commute),
-                              (start: 40, category: .commute),
-                              (start: 80, category: .unknown)].map(toTempTimeSlot)
+        trackEventService.mockEvents = sampleTuples.map(toTrackEvent)
+        
+        let expectedResultTuples : [TempTimeSlotTuple] = [(start: 00, category: .commute),
+                                                          (start: 40, category: .commute),
+                                                          (start: 80, category: .unknown)]
+        
+        let expectedResult = expectedResultTuples.map(toTempTimeSlot)
         
         let generatedTimeslots = self.healthKitTemporaryTimelineGenerator.generateTemporaryTimeline()
         
@@ -79,31 +87,35 @@ class HealthKitTemporaryTimeLineGeneratorTest: XCTestCase
             .forEach { i, actualTimeSlot in compare(timeSlot: actualTimeSlot, to: expectedResult[i]) }
     }
     
-    func testCommuteIsDetectedInsideContinuesWalkingAndRunningSamples()
+    func testCommuteIsDetectedInsideContinuousWalkingAndRunningSamples()
     {
-        trackEventService.mockEvents = [(start: 00, end: 10, identifier: HKQuantityTypeIdentifier.distanceWalkingRunning.rawValue, quantity: 100),
-                                        (start: 10, end: 15, identifier: HKQuantityTypeIdentifier.distanceWalkingRunning.rawValue, quantity: 50),
-                                        (start: 15, end: 20, identifier: HKQuantityTypeIdentifier.distanceWalkingRunning.rawValue, quantity: 200),
-                                        (start: 20, end: 30, identifier: HKQuantityTypeIdentifier.distanceWalkingRunning.rawValue, quantity: 250),
-                                        (start: 30, end: 40, identifier: HKQuantityTypeIdentifier.distanceWalkingRunning.rawValue, quantity: 2),
-                                        (start: 40, end: 50, identifier: HKQuantityTypeIdentifier.distanceWalkingRunning.rawValue, quantity: 200),
-                                        (start: 50, end: 60, identifier: HKQuantityTypeIdentifier.distanceWalkingRunning.rawValue, quantity: 200),
-                                        (start: 60, end: 70, identifier: HKQuantityTypeIdentifier.distanceWalkingRunning.rawValue, quantity: 20),
-                                        (start: 70, end: 80, identifier: HKQuantityTypeIdentifier.distanceWalkingRunning.rawValue, quantity: 50),
-                                        (start: 80, end: 90, identifier: HKQuantityTypeIdentifier.distanceWalkingRunning.rawValue, quantity: 200),
-                                        (start: 90, end: 100, identifier: HKQuantityTypeIdentifier.distanceWalkingRunning.rawValue, quantity: 200),
-                                        (start: 100, end: 110, identifier: HKQuantityTypeIdentifier.distanceWalkingRunning.rawValue, quantity: 4),
-                                        (start: 110, end: 120, identifier: HKQuantityTypeIdentifier.distanceWalkingRunning.rawValue, quantity: 5),
-                                        (start: 120, end: 130, identifier: HKQuantityTypeIdentifier.distanceWalkingRunning.rawValue, quantity: 6)].map(toTrackEvent)
+        let sampleTuples : [HKSampleTuple] = [(start: 00, end: 10, identifier: HKQuantityTypeIdentifier.distanceWalkingRunning.rawValue, quantity: 100),
+                                              (start: 10, end: 15, identifier: HKQuantityTypeIdentifier.distanceWalkingRunning.rawValue, quantity: 50),
+                                              (start: 15, end: 20, identifier: HKQuantityTypeIdentifier.distanceWalkingRunning.rawValue, quantity: 200),
+                                              (start: 20, end: 30, identifier: HKQuantityTypeIdentifier.distanceWalkingRunning.rawValue, quantity: 250),
+                                              (start: 30, end: 40, identifier: HKQuantityTypeIdentifier.distanceWalkingRunning.rawValue, quantity: 2),
+                                              (start: 40, end: 50, identifier: HKQuantityTypeIdentifier.distanceWalkingRunning.rawValue, quantity: 200),
+                                              (start: 50, end: 60, identifier: HKQuantityTypeIdentifier.distanceWalkingRunning.rawValue, quantity: 200),
+                                              (start: 60, end: 70, identifier: HKQuantityTypeIdentifier.distanceWalkingRunning.rawValue, quantity: 20),
+                                              (start: 70, end: 80, identifier: HKQuantityTypeIdentifier.distanceWalkingRunning.rawValue, quantity: 50),
+                                              (start: 80, end: 90, identifier: HKQuantityTypeIdentifier.distanceWalkingRunning.rawValue, quantity: 200),
+                                              (start: 90, end: 100, identifier: HKQuantityTypeIdentifier.distanceWalkingRunning.rawValue, quantity: 200),
+                                              (start: 100, end: 110, identifier: HKQuantityTypeIdentifier.distanceWalkingRunning.rawValue, quantity: 4),
+                                              (start: 110, end: 120, identifier: HKQuantityTypeIdentifier.distanceWalkingRunning.rawValue, quantity: 5),
+                                              (start: 120, end: 130, identifier: HKQuantityTypeIdentifier.distanceWalkingRunning.rawValue, quantity: 6)]
         
-        let expectedResult = [(start: 00, category: .unknown),
-                              (start: 15, category: .commute),
-                              (start: 30, category: .unknown),
-                              (start: 40, category: .commute),
-                              (start: 60, category: .unknown),
-                              (start: 80, category: .commute),
-                              (start: 100, category: .unknown),
-                              (start: 130, category: .unknown)].map(toTempTimeSlot)
+        trackEventService.mockEvents = sampleTuples.map(toTrackEvent)
+        
+        let expectedResultTuples : [TempTimeSlotTuple] = [(start: 00, category: .unknown),
+                                                          (start: 15, category: .commute),
+                                                          (start: 30, category: .unknown),
+                                                          (start: 40, category: .commute),
+                                                          (start: 60, category: .unknown),
+                                                          (start: 80, category: .commute),
+                                                          (start: 100, category: .unknown),
+                                                          (start: 130, category: .unknown)]
+        
+        let expectedResult = expectedResultTuples.map(toTempTimeSlot)
         
         let generatedTimeslots = self.healthKitTemporaryTimelineGenerator.generateTemporaryTimeline()
         
@@ -128,7 +140,7 @@ class HealthKitTemporaryTimeLineGeneratorTest: XCTestCase
         case HKQuantityTypeIdentifier.distanceWalkingRunning.rawValue, HKQuantityTypeIdentifier.distanceCycling.rawValue:
             healthSample = HealthSample(withIdentifier: tuple.identifier, startTime: date(tuple.start), endTime: date(tuple.end), value: HKQuantity(unit: HKUnit.meter(), doubleValue: Double(tuple.quantity)))
         case HKCategoryTypeIdentifier.sleepAnalysis.rawValue:
-            healthSample = HealthSample(withIdentifier: tuple.identifier, startTime: date(tuple.start), endTime: date(tuple.end), value: nil)
+            healthSample = HealthSample(withIdentifier: tuple.identifier, startTime: date(tuple.start), endTime: date(tuple.end), value: HKCategoryValue.init(rawValue: 0))
         default:
             break
         }
